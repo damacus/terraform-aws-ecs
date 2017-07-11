b01f8bcc6e0175f62d95035275d50be4f91cb071
Dan Webb <dan.webb@damacus.io>
2017-07-11 16:23:53 +0100

    use latest

--- module_vpc.tf
+++ module_vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source      = "git@github.com:damacus/terraform-module-vpc.git?ref=v1.0.6"
+  source      = "git@github.com:damacus/terraform-module-vpc.git?ref=master"
   cost_code   = "${var.cost_code}"
   environment = "${terraform.env}"
   owner       = "${var.owner}"
